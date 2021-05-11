e980cacd3252480c680f317d0b9609a2ab5136c7
John Simoneau <john.simoneau@insight.com>
2021-05-10 20:50:08 -0400

    Tried adding module fed in angularjs

--- app/app.module.js
+++ app/app.module.js
@@ -1,5 +1,8 @@
-angular
+import angular from 'angular';
+
+const sample = angular
   .module("simpleApp",[])
   .controller("simpleAppController", ['$scope', function($scope) {
     $scope.hello = "Hello from AngularJS";
   }]);
+export default sample;
--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,24 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: "./app/app.module.js",
   output: {
     uniqueName: "mfe2",
     filename: "bundle.js",
-    path: path.resolve(__dirname, 'dist')
+    // path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: [".webpack.js", ".js"]
   },
   optimization: {
     runtimeChunk: false,
   },
   plugins: [
     new ModuleFederationPlugin({
-      shared: {},
+      shared: {
+        "angular": { singleton: true, eager: true, strictVersion: true }
+      },
       name: "mfe2",
       filename: "mfe2-ngjs.js",
       exposes: {
@@ -22,5 +28,8 @@ module.exports = {
       }
     }),
     new HtmlWebPackPlugin({ template: './index.html'})
-  ]
+  ],
+  devServer: {
+    port: 4350
+  }
 }
